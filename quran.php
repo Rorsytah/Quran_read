5f5b644bd8c3c94e0409f491f8e5962b2445eb7f
Rorsytah <inside345idii@gmail.com>
2019-08-05 16:22:29 +0700

    Initial commit

---
+++ quran.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+
+
+<!-- cdn for bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    </div>
+  </div>
+  <div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent" >
+  </div>
+  <nav class="navbar navbar-light bg-light">
+   <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         English
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">English</a>
+          <a class="dropdown-item" href="#">Cambodia</a>
+          <a class="dropdown-item" href="#">Thailand</a>
+          <a class="dropdown-item" href="#">Malaysia</a>
+          <a class="dropdown-item" href="#">Laos</a>
+          <a class="dropdown-item" href="#">Indonesia</a>
+          <a class="dropdown-item" href="#">Vietnam</a>
+          <a class="dropdown-item" href="#">Brunei</a>
+          <a class="dropdown-item" href="#">Myanmar</a>
+          <a class="dropdown-item" href="#">Philippines</a>
+          <a class="dropdown-item" href="#">Singapore</a>
+          <a class="dropdown-item" href="#">Timor</a>
+
+        </div>
+        <a class="nav-link">Dua Mustajab</a>
+  <nav class="navbar navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
+
+
+
+
+
+
+<div class="_3L7utArEPWqCQEMpZxj92J " style="background-image:url(//assets-1f14.kxcdn.com/images/background.jpg);" data-reactid="20">
+  <div class="container" data-reactid="15">
+     <div class="row" data-reactid="18">
+    <div class="col-md-5 col-md-offset-1 text-center" data-reactid="8">
+  <a class="_1ER6fLboOFYz3PfuJynYyN" data-metrics-event-name="IndexHeader:Link:Index" href="/" data-reactid="20">
+    <img src="//assets-1f14.kxcdn.com/images/logo-lg-w.png" class="_16O6rKQBhfuLJnFgM5ZeXS" alt="logo" data-reactid="4">
+     </a>
+<h2class="_2Ojsf7NyF5aCj3Fvfel3q8" data-reactid="15">THE NOBLE QUR'AN</h2>
+  <form class="form-inline">
+   <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+     <button class="blue blue-blue-success my-2 my-sm-3" type="submit">Search</button>
+  </form></nav></div></div></div></div></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</head>
+</html>
---
+++ test1.php
@@ -0,0 +1,72 @@
+
+
+
+
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+
+
+<!-- cdn for bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+
+
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    </div>
+  </div>
+  <div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent" >
+  </div>
+  <nav class="navbar navbar-light bg-light">
+   <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         English
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">English</a>
+          <a class="dropdown-item" href="#">Cambodia</a>
+          <a class="dropdown-item" href="#">Thailand</a>
+          <a class="dropdown-item" href="#">Malaysia</a>
+          <a class="dropdown-item" href="#">Laos</a>
+          <a class="dropdown-item" href="#">Indonesia</a>
+          <a class="dropdown-item" href="#">Vietnam</a>
+          <a class="dropdown-item" href="#">Brunei</a>
+          <a class="dropdown-item" href="#">Myanmar</a>
+          <a class="dropdown-item" href="#">Philippines</a>
+          <a class="dropdown-item" href="#">Singapore</a>
+
+        </div>
+        <a class="nav-link">Dua Mustajab</a>
+  <nav class="navbar navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+</div>
+
+   
+
+     <header>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+     </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+<div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent" >
+    </div>
+  <nav class="navbar navbar-light bg-light">
+     <header></ul></div></nav></header></div></body><head>
+
+
+
+
+
+</head>
+</html>
